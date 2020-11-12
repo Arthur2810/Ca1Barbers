ab4e1e64f6a9fb260e61031bed8b145c61600c31
Arthur Jardim <62599485+Arthur2810@users.noreply.github.com>
2020-11-12 23:39:29 +0000

    Initial commit

---
+++ src/ca1barbers/Ca1Barbers.java
@@ -0,0 +1,32 @@
+
+package ca1barbers;
+
+import javax.swing.JFrame;
+
+
+public class Ca1Barbers extends JFrame{
+
+
+    public static void main(String[] args) {
+        new Ca1Barbers();
+    }
+    
+    public Ca1Barbers(){
+        
+        this.setVisible(true);
+        this.setSize(300,500);
+        this.setTitle("Barbers");
+        
+        
+        
+        
+        
+        this.validate();
+        this.repaint();
+
+
+
+
+}
+    
+}
